2aa1c2f89b009064ec8c3c46a85a36a9e347b06a
Imran <imran789924@gmail.com>
2020-02-16 12:53:31 +0600

    Logistic Regression - 80% accuracy

--- solution.py
+++ solution.py
@@ -19,6 +19,30 @@ X.drop(X.columns[[0, 2, 7, 9]], axis = 1, inplace = True)
 X = pd.get_dummies(X)
 X.drop(X.columns[[6, 9]], axis = 1, inplace = True)
 
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+X = imputer.fit_transform(X) 
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=0)
+
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10)
+accuracies.mean()
+accuracies.std()
+
+
 '''
 features = ["Pclass", "Sex", "SibSp", "Parch"]
 Xor = pd.get_dummies(dataset[features])
